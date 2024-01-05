90dd531577c0a303e84677fbfbb4d1d68313b2c3
jrajasuresh <jrajasuresh@gmail.com>
2024-01-05 16:22:41 +0530

    Update HelloController.java

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-	@GetMapping("/")
+	@GetMapping("/test")
 	public String index() {
 		return "Greetings from Spring Boot! Check recommit";
 	}
